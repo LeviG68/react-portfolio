4773cc0657f517bcdc2ec22c226aaafcfcd529d7
levi <glendenning.levi@gmail.com>
2020-06-05 20:17:14 -0600

    change server file

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, '../src/index.html'));
+    res.sendFile(path.join(publicPath, '../index.html'));
 });
 
 app.listen(port, () => {
