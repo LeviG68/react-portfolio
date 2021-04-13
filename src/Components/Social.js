c115fa04f4a5622a183e218ceff07d42e7681bf8
levi <glendenning.levi@gmail.com>
2021-04-13 16:30:32 -0600

    material-ui icons and grid layout for social.js conponent

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import GitHubIcon from '@material-ui/icons/GitHub';
 
+//import of Material-ui icons, themes and colors
+import { ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+const customTheme = createMuiTheme ({
+    spacing: 2,
+    palette: {
+        primary: {
+            main: '#1DA1F2'
+        },
+        secondary: {
+            main: '#211F1F'
+        },
+        accent: {
+            main: '#2867B2'
+        },
+    },
+    root: {
+        paddingLeft: '20px',
+        paddingright: '20px',
+        display: 'grid',
+        
+
+    },
+});
 
 class Social extends Component {
 
     render() {
 
+        // const theme = customTheme();
+        
         return (
+            <Grid container spacing={1} justify="center">
+                <ThemeProvider theme={customTheme}>
+                    <Grid item xs={1}>
+                        <MailOutlineIcon fontSize="large" spacing={4} className />
+                    </Grid>
+                    <Grid item xs={1}>
+                        <GitHubIcon  fontSize='large' color='secondary' className='github'/>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <TwitterIcon fontSize='Large' color='primary' className='twitter'/>
+                    </Grid>
+                    <Grid item xs={1}>
+                    <LinkedInIcon fontSize='Large' color='accent' className='linkedin' />
+                    </Grid>
 
-            <div className='social'>
+                </ThemeProvider>
+            </Grid>
             
-                <a href="https://github.com/LeviG68" className= "github social"> <FontAwesomeIcon icon={faGithub} size='2x'/>
-                </a>
-                <a href="https://www.linkedin.com/in/levi-glendenning/" className="linkedin social"><FontAwesomeIcon icon={faLinkedin} size='2x'/>
-                </a>
-                <a href="https://twitter.com/@DevopPeachfuzz" className= "twitter social"><FontAwesomeIcon icon={faTwitter} size='2x' />
-                </a>
-                
-
-               
-            </div>
         )
     }
 }
