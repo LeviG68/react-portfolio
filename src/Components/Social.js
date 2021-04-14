52ae20944912eb055562b071475643f3ecaafacd
levi <glendenning.levi@gmail.com>
2021-04-13 22:06:23 -0600

    finished added icons with spacing and button links

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 //import of Material-ui icons, themes and colors
 import { ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -10,24 +11,10 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const customTheme = createMuiTheme ({
-    spacing: 2,
-    palette: {
-        primary: {
-            main: '#1DA1F2'
-        },
-        secondary: {
-            main: '#211F1F'
-        },
-        accent: {
-            main: '#2867B2'
-        },
-    },
     root: {
         paddingLeft: '20px',
         paddingright: '20px',
-        display: 'grid',
-        
-
+        display: 'grid'
     },
 });
 
@@ -41,16 +28,27 @@ class Social extends Component {
             <Grid container spacing={1} justify="center">
                 <ThemeProvider theme={customTheme}>
                     <Grid item xs={1}>
-                        <MailOutlineIcon fontSize="large" spacing={4} className />
+                        <Button>
+                            <MailOutlineIcon fontSize="large" spacing={4} href='mailto:glendenning.levi@gmail.com'/>
+                        </Button>    
                     </Grid>
+
                     <Grid item xs={1}>
-                        <GitHubIcon  fontSize='large' color='secondary' className='github'/>
+                        <Button>
+                            <GitHubIcon  fontSize='large' style={{color:'#211F1F'}} href=''/>
+                        </Button>
                     </Grid>
+
                     <Grid item xs={1}>
-                        <TwitterIcon fontSize='Large' color='primary' className='twitter'/>
+                        <Button>
+                            <TwitterIcon fontSize='Large' style={{color:'#1DA1F2'}} />
+                        </Button>
                     </Grid>
+
                     <Grid item xs={1}>
-                    <LinkedInIcon fontSize='Large' color='accent' className='linkedin' />
+                        <Button>
+                            <LinkedInIcon fontSize='Large'  style={{color:'#2867B2'}} />
+                        </Button>
                     </Grid>
 
                 </ThemeProvider>
